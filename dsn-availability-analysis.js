cd3dda1b5b8a0f88f2d6da6c2ac659e0ca003df8
angeli-adobe <72991519+angeli-adobe@users.noreply.github.com>
2024-04-18 14:23:52 +0200

    Export to AEP schema format
    Added a function that export the full data and the delta data into a JSON file with schema format.

--- dsn-availability-analysis.js
+++ dsn-availability-analysis.js
@@ -20,7 +20,8 @@ function readDataFromFile() {
 }
 
 // Function to replace the "value" node in dsn-availability.json with an array of objects and merge with the old data
-function replaceValueNode(jsonData) {
+//  Type parameter define whether return the full or delta data.
+function convertToArray(jsonData) {
     let out = [];
     const keys = Object.keys(jsonData.value);
     for (let i = 0; i < keys.length; i++) {
@@ -35,21 +36,15 @@ function replaceValueNode(jsonData) {
         out.push(value);
     }
     out = _.sortBy(out, function (v) { return v.timestamp });
-
-    // Read the file source_array.json
-    const oldData = JSON.parse(fs.readFileSync('input/dsn-availability/source_array.json', 'utf8'));
-
-    // Merge old data and out and return a deduplicated array
-    let union = _.uniq(_.union(oldData, out),function (el) { return el.testid; });
-    // console.log(oldData.length)
-    // console.log(out.length)
-    // console.log(union.length)
-
-    // Write union in a file called source_array.json
-    fs.writeFileSync('input/dsn-availability/source_array.json', JSON.stringify(union,null, 4));
-
     return out;
+}
 
+function mergeHistoricalData(newData, oldData) {
+    // Merge old data and out and return a deduplicated array
+    let union = _.uniq(_.union(oldData, newData), function (el) { return el.testid; });
+    // Write union in a file called source_array_full.json
+    fs.writeFileSync('input/dsn-availability/source_array_full.json', JSON.stringify(union, null, 4));
+    return union;
 }
 
 function convertToDateTime(str) {
@@ -66,11 +61,87 @@ function convertToDateTime(str) {
 
 // Main function that calls the other functions
 function main() {
-    let filename = 'dsn-availability-results' + '.json'
-    const jsonData = readDataFromFile();
-    const updatedValue = replaceValueNode(jsonData);
-    const dataAnalysis = dataAnalyser(updatedValue);
-    fs.writeFileSync('output/dsn-availability/'+ filename, JSON.stringify(dataAnalysis, 0 , 4));
+
+    const mostRecentData = readDataFromFile();
+    // Read the file source_array.json
+    const oldData = JSON.parse(fs.readFileSync('input/dsn-availability/source_array_full.json', 'utf8'));
+
+    let dataForAEPFull = 'data-for-aep_full' + '.json';
+    const mostRecentDataArray = convertToArray(mostRecentData);
+    // Merge mostRecentDataArray with history
+    const fullData = mergeHistoricalData(mostRecentDataArray, oldData);
+    const schemaOutputFull = createSchemaOutput(fullData);
+    fs.writeFileSync('output/dsn-availability/' + dataForAEPFull, JSON.stringify(schemaOutputFull, 0, 4));
+
+    let dataForAEPDelta = 'data-for-aep_delta' + '.json';
+    const deltaData = getDeltaData(mostRecentDataArray,oldData);
+    const schemaOutputDelta = createSchemaOutput(deltaData);
+    fs.writeFileSync('output/dsn-availability/' + dataForAEPDelta, JSON.stringify(schemaOutputDelta, 0, 4));
+}
+
+function getDeltaData(newData, oldData){
+    let delta =  newData.filter(newItem => {
+        return !oldData.some(oldItem => newItem.testid === oldItem.testid)
+      });
+      fs.writeFileSync('input/dsn-availability/source_array_delta.json', JSON.stringify(delta, null, 4));
+    return delta;
+}
+
+function createSchemaOutput(data) {
+    let out = [];
+
+    // DatacenterID
+    let datacenterID = "luma-emea";
+
+    // Read the file schema.json
+    const schema = JSON.parse(fs.readFileSync('input/dsn-availability/schema.json', 'utf8'));
+
+    // Loop through data
+    for (let i = 0; i < data.length; i++) {
+        const d = data[i];
+
+        // Event collection
+        //  With underscore clone the schema object
+        let clone_a = JSON.parse(JSON.stringify(schema));
+        // Convert data.dateString to ISO 8601 format with moment
+        clone_a.timestamp = moment(d.dateString, 'MMMM Do YYYY, h:mm:ss a').toISOString();
+        clone_a.eventType = "test.completed";
+        clone_a._demopotemea.identities.datacenterId = datacenterID;
+        clone_a._demopotemea.testDetails.testType = 'event-collection';
+        clone_a._demopotemea.testDetails.testResult.testPass = d["event-collection"] == "pass" ? 1 : 0;
+        clone_a._demopotemea.testDetails.testResult.testWarn = d["event-collection"] == "warn" ? 1 : 0;
+        clone_a._demopotemea.testDetails.testResult.testFail = d["event-collection"] == "fail" ? 1 : 0;
+        out.push(clone_a);
+
+        // Segmentation
+        //  With underscore clone the schema object
+        let clone_b = JSON.parse(JSON.stringify(schema));
+        // Convert data.dateString to ISO 8601 format with moment
+        clone_b.timestamp = moment(d.dateString, 'MMMM Do YYYY, h:mm:ss a').toISOString();
+        clone_b.eventType = "test.completed";
+        clone_b._demopotemea.identities.datacenterId = datacenterID;
+        clone_b._demopotemea.testDetails.testType = 'segmentation';
+        clone_b._demopotemea.testDetails.testResult.testPass = d["segmentation"] == "pass" ? 1 : 0;
+        clone_b._demopotemea.testDetails.testResult.testWarn = d["segmentation"] == "warn" ? 1 : 0;
+        clone_b._demopotemea.testDetails.testResult.testFail = d["segmentation"] == "fail" ? 1 : 0;
+        out.push(clone_b);
+
+        // Profile collection
+        //  With underscore clone the schema object
+        let clone_c = JSON.parse(JSON.stringify(schema));
+        // Convert data.dateString to ISO 8601 format with moment
+        clone_c.timestamp = moment(d.dateString, 'MMMM Do YYYY, h:mm:ss a').toISOString();
+        clone_c.eventType = "test.completed";
+        clone_c._demopotemea.identities.datacenterId = datacenterID;
+        clone_c._demopotemea.testDetails.testType = 'profile-collection';
+        clone_c._demopotemea.testDetails.testResult.testPass = d["profile-collection"] == "pass" ? 1 : 0;
+        clone_c._demopotemea.testDetails.testResult.testWarn = d["profile-collection"] == "warn" ? 1 : 0;
+        clone_c._demopotemea.testDetails.testResult.testFail = d["profile-collection"] == "fail" ? 1 : 0;
+        out.push(clone_c);
+
+    }
+
+    return out;
 }
 
 function dataAnalyser(data) {
